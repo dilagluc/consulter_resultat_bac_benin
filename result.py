3c2f6559c73c25877010c0b22ce1d19a707ae6b5
dilag-luc <61668945+dilag-luc@users.noreply.github.com>
2020-04-07 20:07:29 +0000

    Create result.py

---
+++ result.py
@@ -0,0 +1,62 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+# Desc: Obtenez les dernières statistiques du Bénin sur le coronavirus directement en ligne de commande
+# Author: https://twitter.com/dilag_luc
+                                                                  
+import sys
+reload(sys)
+sys.setdefaultencoding('utf8')
+import urllib2
+import urllib
+from PIL import Image
+import ClientForm
+from bs4 import BeautifulSoup
+import datetime
+class cli_colors:
+    HEADER = '\033[95m'
+    OKBLUE = '\033[94m'
+    OKGREEN = '\033[92m'
+    WARNING = '\033[93m'
+    FAIL = '\033[91m'
+    ENDC = '\033[0m'
+    BOLD = '\033[1m'
+    UNDERLINE = '\033[4m'
+    
+response=urllib2.urlopen(urllib2.Request("http://www.officedubacbenin.bj/site/spip.php?page=resultats_bac2"))
+forms = ClientForm.ParseResponse(response)
+response.close()
+form =forms[1]
+#print form
+resultat=raw_input("Numéro de table : ")
+form["id_table"]=resultat
+re=raw_input("Année d'obtention : ")
+form["annee"]=[re]
+#request2= form.click()
+response2=urllib2.urlopen(form.click())
+document= urllib.urlopen(response2.geturl())
+bs=BeautifulSoup(document.read(),"lxml")
+search=bs.find_all('font')
+
+def sysout(output=''):
+    sys.stdout.write(output + '\n')
+    
+case = []
+for i in range(len(search)):
+    hh=search[i].string
+    case.append(hh)
+
+
+sysout("\n")
+sysout(cli_colors.FAIL + 'Noms et prénoms\t: ' + '{}'.format(case[1]))
+sysout(cli_colors.WARNING + 'Numéro de table\t: ' + '{}'.format(case[2]))
+sysout(cli_colors.OKGREEN + 'Série\t\t: ' + '{}'.format(case[3]))
+sysout(cli_colors.HEADER + 'Jury\t\t: ' + '{}'.format(case[4]))
+sysout(cli_colors.OKBLUE + 'Décision\t: ' + '{}'.format(case[5]))
+sysout(cli_colors.ENDC + 'Lien\t\t: ' + '{}'.format(response2.geturl()))
+sysout("\nSource: www.officedubacbenin.bj ---- Date : {}".format(datetime.datetime.now().strftime("%m/%d/%Y, %H:%M:%S")))
+hh=bs.find_all('img')
+url="http://www.officedubacbenin.bj"+hh[1]['src']
+urllib.urlretrieve(url,"0001.jpg")
+img = Image.open('./0001.jpg')
+img.show()
